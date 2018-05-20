38a256de71c811c9dd31ef1c054ec46c09863be0
Thom <thomasrossi1@gmail.com>
2018-05-20 19:56:15 +0200

    Revert "Fixed size transmission problems"
    This reverts commit f0a4b1a69050e194af0494e385a351b2bb710083.

--- arduino.py
+++ arduino.py
@@ -12,44 +12,35 @@ class Arduino():
         
         ''' Welcome sequence '''
         seq = [0, 255, 0]
-        rec_char = '1'
+        rec_char = -1
         for i in range(len(seq)):
             print("Waiting for welcome char :", seq[i], "...")
             while ord(rec_char) != seq[i]:
                 rec_char = self.serial.read(1)
-                if(rec_char == b''): rec_char = '1'
-                
         print("Welcomed !\n")
         
         ''' Commands '''
         self.DONE = 100
-        self.END = 99
-        
         self.PIN_MODE = 101
         self.DIGITAL_WRITE = 102
         self.DIGITAL_READ = 103
         self.ANALOG_WRITE = 104
         self.ANALOG_READ = 105
         
-        self.SEQUENCE_PWM = 106
-        
         ''' Defines '''
         self.LOW = 0
         self.HIGH = 1
         self.INPUT = 0
         self.OUTPUT = 1
-        
-        self.toByte = lambda n: bytes(chr(n), 'utf-8')
     
-    def sendCommand(self, commandId, args = [], waitForDone = False):
-        #print(self.toByte(commandId))
-        self.serial.write(self.toByte(commandId))
+    def sendCommand(self, commandId, args, waitForDone = False):
+        self.serial.write(chr(commandId))
         
         for arg in args:
-            self.serial.write(self.toByte(arg))
+            self.serial.write(chr(arg))
         
         if waitForDone:
-            rec_char = '1'
+            rec_char = -1
             while ord(rec_char) != self.DONE:
                 rec_char = self.serial.read(1)
     
@@ -61,34 +52,19 @@ class Arduino():
         
     def digitalRead(self, pin):
         self.sendCommand(self.DIGITAL_READ, [pin])
-        return self.serial.read(1)
+        return ord(self.serial.read(1))
         
     def analogWrite(self, pin, value):
-        if value > 255: value = 255
-        elif value < 0: value = 0
-        # The 8 most significant bits are written separated of the 8 last, to keep ascii-compatible values.
-        self.sendCommand(self.ANALOG_WRITE, [pin, (value >> 8) & 0xFF, value & 0xFF], True)
+        self.sendCommand(self.ANALOG_WRITE, [pin, value], True)
         
     def analogRead(self, pin):
         self.sendCommand(self.ANALOG_READ, [pin])
         
-        valueMSB = ord(self.serial.read(1))
-        valueLSB = ord(self.serial.read(1))
+        char1 = ord(self.serial.read(1))
+        char2 = ord(self.serial.read(1))
         
-        return valueMSB * 0x100 + valueLSB
-    
-    
-    def sequencePWM(self, datas, size):
-        self.sendCommand(self.SEQUENCE, waitForDone = True)
-        self.serial.write(self.toByte(size))
-        
-        for data in datas:
-            self.serial.write(self.toByte(data))
-    
-    def end(self):
-        self.sendCommand(self.END)
-        self.close()
+        return char1 * 0x100 + char2
 
     def close(self):
-        self.serial.close()
+        self.ser.close()
                 
--- interface/interface.ino
+++ interface/interface.ino
@@ -1,47 +1,46 @@
 /* Commands */
-#define DONE 100
-#define END 99
+  #define DONE 100
 
-#define PIN_MODE 101
-#define DIGITAL_WRITE 102
-#define DIGITAL_READ 103
-#define ANALOG_WRITE 104
-#define ANALOG_READ 105
-#define SEQUENCE_PWM 106
+  #define PIN_MODE 101
+  #define DIGITAL_WRITE 102
+  #define DIGITAL_READ 103
+  #define ANALOG_WRITE 104
+  #define ANALOG_READ 105
 
 
 void setup() {
   Serial.begin(115200);
   Serial.flush(); // Flushes the Serial buffer
-
+  
   /* Initializing communication */
-  Serial.write(0);
-  Serial.write(255);
-  Serial.write(0);
+  Serial.write((char) 0);
+  Serial.write((char) 255);
+  Serial.write((char) 0);
 }
 
 void loop() {
   char command = 0;
-  if (Serial.available() > 0)   { // Blocks the loop while no PC instruction is detected
+  if(Serial.available() > 0) { // Blocks the loop while no PC instruction is detected
     command = Serial.read();
-
-    switch (command) {
+    
+    switch(command) {
       case PIN_MODE:      cmd_pinMode();      break;
       case DIGITAL_WRITE: cmd_digitalWrite(); break;
       case DIGITAL_READ:  cmd_digitalRead();  break;
       case ANALOG_WRITE:  cmd_analogWrite();  break;
       case ANALOG_READ:   cmd_analogRead();   break;
-      case SEQUENCE_PWM:  cmd_sequencePWM();  break;
-      case END:           cmd_end();          break;
-    }
 
-    Serial.flush();
+      default: 
+        Serial.print("Unknown command : "); 
+        Serial.print(command); 
+        Serial.print("\n");
+    }
   }
 
 }
 
 void wait_args(unsigned int nbr = 1) {
-  while (Serial.available() < nbr);
+  while(Serial.available() < nbr);
 }
 
 void done() {
@@ -85,54 +84,24 @@ void cmd_digitalRead() {
 
 /* Arduino syntax : analogWrite(pin, value) */
 void cmd_analogWrite() {
-  char pin = 0, valueMSB = 0, valueLSB = 0;
-  wait_args(3);
+  char pin = 0, value = 0;
+  wait_args(2);
 
   pin = Serial.read();
-  valueMSB = Serial.read();
-  valueLSB = Serial.read();
+  value = Serial.read();
 
-  analogWrite(pin, valueMSB * 0x100 + valueLSB);
+  analogWrite(pin, value);
   done();
 }
 
 /* Arduino syntax : analogRead(pin) */
 void cmd_analogRead() {
-  char pin = 0;
-  wait_args(1);
+   char pin = 0;
+   wait_args(1);
 
-  char value = analogRead(pin); // Coded on 10 bits !
+   value = analogRead(pin); // Coded on 10 bits !
 
-  Serial.write( (value >> 8) & 0xFF ) ;  // 8 first bits (that are thus shifted 8 spots to the right before being compared to 0xFF)
-  Serial.write( value & 0xFF );          // 8 last bits of the value
-}
-
-
-void cmd_sequencePWM() {
-  char n = 0; // Data size in number of variables transmitted
-  char *datas;
-  done(); // Tells the PC the arduino is ready to receive datas
-
-  wait_args(1); // Waits for the data size
-  n = Serial.read();
-
-  datas = (char *) malloc(n);
-  wait_args(n);
-
-  for (int i = 0; i < n; i++) {
-    datas[i] = Serial.read();
-  }
-
-  /* Executing PWM */
-
-}
-
-void cmd_end() {
-  Serial.flush(); // Flushes the Serial buffer
-
-  /* Initializing communication */
-  Serial.write(0);
-  Serial.write(255);
-  Serial.write(0);
+   Serial.write( (value >> 8) & 0xFF ) ;  // 8 first bits (that are thus shifted 8 spots to the right before being compared to 0xFF)
+   Serial.write( value & 0xFF );          // 8 last bits of the value
 }
 
